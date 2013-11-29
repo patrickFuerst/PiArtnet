2b6e86498e3c4881c3560225a148f5f99d270257
Patrick Fürst <pat.fuerst@gmail.com>
2013-11-29 12:05:57 +0100

    timefound added

--- src/TimeEvent.java
+++ src/TimeEvent.java
@@ -16,12 +16,15 @@ public class TimeEvent {
 	 * The value send to the channel
 	 */
 	
+	private double timeFound;
+	
 	private int value;
 
 	public TimeEvent(int time, int channel, int value) {
 		this.time = time;
 		this.channel = channel;
 		this.value = value;
+		this.timeFound = 0;
 		
 		
 	}
@@ -48,6 +51,14 @@ public class TimeEvent {
 
 	public void setValue(int value) {
 		this.value = value;
+	}
+
+	public double getTimeFound() {
+		return timeFound;
+	}
+
+	public void setTimeFound(double timeFound) {
+		this.timeFound = timeFound;
 	} 
 	
 	
